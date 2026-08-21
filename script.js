27208b817a300141bf9ee7afbb83ed6b4a07b2a5
MARK RYAN SIMBULAN <simbulanmarkryan@gmail.com>
2026-08-21 11:18:51 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,43 @@
+const weddingDate = new Date("November 26, 2026 15:30:00").getTime();
+
+function updateCountdown() {
+
+    const now = new Date().getTime();
+
+    const difference = weddingDate - now;
+
+    if (difference <= 0) {
+
+        document.getElementById("days").textContent = "0";
+        document.getElementById("hours").textContent = "0";
+        document.getElementById("minutes").textContent = "0";
+        document.getElementById("seconds").textContent = "0";
+
+        return;
+    }
+
+    const days = Math.floor(
+        difference / (1000 * 60 * 60 * 24)
+    );
+
+    const hours = Math.floor(
+        (difference / (1000 * 60 * 60)) % 24
+    );
+
+    const minutes = Math.floor(
+        (difference / (1000 * 60)) % 60
+    );
+
+    const seconds = Math.floor(
+        (difference / 1000) % 60
+    );
+
+    document.getElementById("days").textContent = days;
+    document.getElementById("hours").textContent = hours;
+    document.getElementById("minutes").textContent = minutes;
+    document.getElementById("seconds").textContent = seconds;
+}
+
+updateCountdown();
+
+setInterval(updateCountdown, 1000);
